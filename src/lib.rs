fdcb5a04e1fde5ea9f0d90efab9b870e76db8570
Hyeon Kim <simnalamburt@gmail.com>
2015-07-28 07:28:11 +0900

    Basic implementation
    Copied from patch of @alexcrichton
    
    Reference:
      https://github.com/rust-lang/cargo/commit/23762e4586#diff-b99116b24344572ec62bb4702e213e50R119

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,55 @@
-#[test]
-fn it_works() {
+#[cfg(unix)]
+extern crate libc;
+
+use std::path::Path;
+use std::io;
+
+#[cfg(unix)]
+pub fn set_file_times(path: &Path, atime: u64, mtime: u64) -> io::Result<()> {
+    use std::os::unix::prelude::*;
+    use std::ffi::CString;
+    use libc::{timeval, time_t, c_char, c_int};
+    extern {
+        fn utimes(name: *const c_char, times: *const timeval) -> c_int;
+    }
+
+    let path = try!(CString::new(path.as_os_str().as_bytes()));
+    let atime = timeval { tv_sec: atime as time_t, tv_usec: 0, };
+    let mtime = timeval { tv_sec: mtime as time_t, tv_usec: 0, };
+    let times = [atime, mtime];
+    let ret = unsafe { utimes(path.as_ptr(), times.as_ptr()) };
+
+    if ret == 0 {
+        Ok(())
+    } else {
+        Err(io::Error::last_os_error())
+    }
+}
+
+#[cfg(windows)]
+pub fn set_file_times(path: &Path, atime: u64, mtime: u64) -> io::Result<()> {
+    use std::fs::OpenOptions;
+    use std::os::windows::prelude::*;
+    use winapi::{FILETIME, DWORD};
+    use kernel32::SetFileTime;
+
+    let f = try!(OpenOptions::new().write(true).open(p));
+    let atime = to_filetime(atime);
+    let mtime = to_filetime(mtime);
+    let ret = unsafe { SetFileTime(f.as_raw_handle() as *mut _, 0 as *const _, &atime, &mtime) };
+
+    if ret != 0 {
+        Ok(())
+    } else {
+        Err(io::Error::last_os_error())
+    }
+
+    // FILETIME is a count of 100ns intervals, and there are 10^7 of these in a second
+    fn to_filetime(seconds: u64) -> FILETIME {
+        let seconds = seconds * 10_000_000;
+        FILETIME {
+            dwLowDateTime: seconds as DWORD,
+            dwHighDateTime: (seconds >> 32) as DWORD,
+        }
+    }
 }
