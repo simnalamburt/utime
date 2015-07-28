e8b16ad5713c127dae2e2b0c61f81d2e2bd7ef20
Hyeon Kim <simnalamburt@gmail.com>
2015-07-28 18:17:58 +0900

    Fix compile error

--- src/lib.rs
+++ src/lib.rs
@@ -33,8 +33,8 @@ fn utime<P: AsRef<Path>>(path: P, atime: u64, mtime: u64) -> io::Result<()> {
     let atime = timeval { tv_sec: atime as time_t, tv_usec: 0, };
     let mtime = timeval { tv_sec: mtime as time_t, tv_usec: 0, };
     let times = [atime, mtime];
-    let ret = unsafe { utimes(path.as_ptr(), times.as_ptr()) };
 
+    let ret = unsafe { utimes(path.as_ptr(), times.as_ptr()) };
     if ret == 0 {
         Ok(())
     } else {
@@ -52,13 +52,6 @@ fn utime<P: AsRef<Path>>(path: P, atime: u64, mtime: u64) -> io::Result<()> {
     let f = try!(OpenOptions::new().write(true).open(path));
     let atime = to_filetime(atime);
     let mtime = to_filetime(mtime);
-    let ret = unsafe { SetFileTime(f.as_raw_handle() as *mut _, 0 as *const _, &atime, &mtime) };
-
-    if ret != 0 {
-        Ok(())
-    } else {
-        Err(io::Error::last_os_error())
-    }
 
     // FILETIME is a count of 100ns intervals, and there are 10^7 of these in a second
     fn to_filetime(seconds: u64) -> FILETIME {
@@ -67,5 +60,12 @@ fn utime<P: AsRef<Path>>(path: P, atime: u64, mtime: u64) -> io::Result<()> {
             dwLowDateTime: seconds as DWORD,
             dwHighDateTime: (seconds >> 32) as DWORD,
         }
+    }
+
+    let ret = unsafe { SetFileTime(f.as_raw_handle() as *mut _, 0 as *const _, &atime, &mtime) };
+    if ret != 0 {
+        Ok(())
+    } else {
+        Err(io::Error::last_os_error())
     }
 }
